d36176fa3514354f8762cae13e8ea5c67790970f
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2025-02-03 11:53:32 -0300

    feat: foi adicionado a model da classe Room #1

---
+++ src/main/java/com/luizguilherme/meeting_management/model/Room.java
@@ -0,0 +1,25 @@
+package com.luizguilherme.meeting_management.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    private String resources;
+}
